ba6e8368382a0de0ca15b32cc662ff812bc5f4da
saamiahmed <saamiahmed@users.noreply.github.com>
2015-04-23 18:35:11 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require 'person.php';
+
+$person = new person;
+echo $person ->firstName = "Sav"; 
+
+require 'address.php';
+
+$address = new Address;
+echo $address -> city ='Mankato';
+
+?>
